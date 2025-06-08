949747e8180619907f2c4c5be3e769c11bf83a7e
Antipas Zemba <antipasnzeba@gmail.com>
2025-06-08 00:57:56 -0700

    # User & Question classes

--- app.py
+++ app.py
@@ -4,3 +4,22 @@ from tkinter import ttk
 import time
 import random
 
+# User & Question classes
+class User:
+    def __init__(self, first_name, last_name, id_number, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
+        self.age = age
+
+class Question:
+    def __init__(self, prompt_en, prompt_fr, options_en, options_fr, correct_option, category):
+        self.prompt_en = prompt_en
+        self.prompt_fr = prompt_fr
+        self.options_en = options_en
+        self.options_fr = options_fr
+        self.correct_option = correct_option.upper()
+        self.category = category
+
+    def is_correct(self, answer):
+        return answer.upper() == self.correct_option
